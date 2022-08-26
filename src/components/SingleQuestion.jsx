be9b3229c9741f4ad19d51e77ea67a54f9db91aa
AhlamKhalid <ahlamkarkashan@gmail.com>
2022-08-27 00:01:22 +0300

    decode entities

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -1,15 +1,17 @@
+import { decode } from "html-entities";
+
 function SingleQuestion(props) {
   const answersElements = props.allAnswers.map((answer, index) => {
     return (
       <button key={index} className="answer-btn">
-        {answer}
+        {decode(answer)}
       </button>
     );
   });
 
   return (
     <div className="question-container">
-      <h1 className="question">{props.question}</h1>
+      <h1 className="question">{decode(props.question)}</h1>
       <div className="answers-btn-container">{answersElements}</div>
     </div>
   );
