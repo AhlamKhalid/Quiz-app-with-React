5d1ee72d48c8a544413d20c4c65ab72c195b4ff5
AhlamKhalid <ahlamkarkashan@gmail.com>
2022-08-26 19:41:30 +0300

    shuffle answers

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -8,8 +8,36 @@ function Questions() {
       .then((response) => response.json())
       .then((data) => {
         setQuestions(data.results);
+        const firstQuestion = data.results[0];
+        console.log(firstQuestion);
+        const allAnswers = [
+          ...firstQuestion.incorrect_answers,
+          firstQuestion.correct_answer
+        ];
+        console.log(allAnswers);
+        const shuffledAnswers = shuffle(allAnswers);
+        console.log(shuffledAnswers);
       });
   }, []);
+
+  // function to shuffle answers
+  // https://stackoverflow.com/a/2450976
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex]
+      ];
+    }
+
+    return array;
+  }
 
   return (
     <div>
